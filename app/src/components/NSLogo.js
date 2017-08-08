148bb719d6502e8d4388c38913ec3dc752dbeb6c
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-08-08 15:36:51 +0800

    change logo source from png to svg

--- app/src/components/NSLogo.js
+++ app/src/components/NSLogo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import nslogo from '../../assets/images/nslogo.png';
+import nslogo from '../../assets/images/header_ns_logo.svg';
 
 class NSLogo extends React.Component {
     render() {
         return (
-        	<div className="wrapper">
-        		<img id="nslogo" src={nslogo} alt="Naughty Swing"/>
+        	<div className="nslogo-container">
+        		<img className='nslogo-img' id="nslogo" src={nslogo} alt="Naughty Swing"/>
          	</div>
         );
     }
