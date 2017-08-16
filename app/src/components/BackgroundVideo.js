3a895b53f579f5b9ebd6331cc3d8059bfec38bb3
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-08-16 10:55:13 +0800

    merge from branch

---
+++ app/src/components/BackgroundVideo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Youtube from 'react-youtube';
+
+class BackgroundVideo extends React.Component {
+    render() {
+        const videoId = 'SYE4T7YvZ7U';
+        const opt = {
+            width: 'auto',
+            height: 'auto',
+            playerVars: {
+                list:'PLwOAbLN_Fhuc2QadYdfWXdAwAWcUGqa4d',
+                listType: 'playlist',
+                autoplay: true,
+                loop: 1,
+                controls: 0,
+                disablekb: true,
+                rel:0,
+                enablejsapi: true,
+                showinfo: 0
+            }
+        };
+
+        return(
+            <div className="backgroundvideo-container">
+                <Youtube
+                    videoId={ videoId }          
+                    id={'mainvideo'}
+                    opts={opt}
+                    onReady={(e)=>{
+                        e.target.mute();
+                        e.target.setLoop(true);
+                    }}
+                />
+            </div>
+        );
+    }
+}
+
+export default BackgroundVideo;
