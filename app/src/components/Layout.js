51d392ad3cc3dc051a50944fcf51d542fcafaec5
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-08-29 17:29:45 +0800

    add layout component refactoring routers

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import Header from './Header';
-import MediaQuery from 'react-responsive';
+import Footer from './Footer';
 
 class Layout extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			videoStatus: 'loading'
-		};
-
-		this.handleVideoLoaded = this.handleVideoLoaded.bind(this);
-		this.handleVideoErrored = this.handleVideoErrored.bind(this);
-
-	}
-
-	handleVideoLoaded(){
-		console.log('video loaded');
-    	this.setState({ videoStatus: 'loaded' });
-    }
-
-    handleVideoErrored(){
-    	console.log('failed to load video');
-    	this.setState({ videoStatus: 'failed to load' });
-    }
-
-
 	render() {
 		return(
 			<div id="container">
 				<Header />
-				<MediaQuery minDeviceWidth={1224}>
-					<iframe width="1280" height="720" src="https://www.youtube.com/embed/SYE4T7YvZ7U?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-				</MediaQuery>
 				<div className="content">
 					{ this.props.children }
 				</div>
+				<Footer />
 			</div>
 		);
 	}
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import Header from '../components/Header';
 import Layout from '../components/Layout';
 import IndexPage from '../components/IndexPage';
 import AboutUs from '../components/AboutUs';
@@ -9,7 +8,6 @@ import Portfolio from '../components/Portfolio';
 import Cooperative from '../components/Cooperative';
 import Classes from '../components/Classes';
 import SwingEvents from '../components/SwingEvents';
-import Footer from '../components/Footer';
 import NotFoundPage from '../components/NotFoundPage';
 
 class AppRoutes extends React.Component{
@@ -17,112 +15,17 @@ class AppRoutes extends React.Component{
 		let {history} = this.props;
 		return(
 			<Router history={history}>
-				<Switch>
-					<Route exact path="/" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0); 
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<IndexPage />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/about" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<AboutUs />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/portfolio" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<Portfolio />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/cooperative" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<Cooperative />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/classes" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<Classes />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/swingevents" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<SwingEvents />
-									</div>
-									<Footer />
-								</div>
-							</div>
-						)
-					}}/>
-					<Route path="/*" component={()=>{
-						// TODO: find a better way
-						window.scrollTo(0,0);
-						return(
-							<div>
-								<div id="container">
-									<Header />
-									<div className="content">
-										<NotFoundPage />
-									</div>
-								</div>
-							</div>
-						)
-					}}/>
-				</Switch>
+				<Layout>
+					<Switch>
+						<Route exact path="/" component={IndexPage}/>
+						<Route path="/about" component={AboutUs}/>
+						<Route path="/portfolio" component={Portfolio}/>
+						<Route path="/cooperative" component={Cooperative}/>
+						<Route path="/classes" component={Classes}/>
+						<Route path="/swingevents" component={SwingEvents}/>
+						<Route path="/*" component={NotFoundPage}/>
+					</Switch>
+				</Layout>
 			</Router>
 		);
 	}
