df4daa1461361988295fe68804c0daeab9dc1747
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-09-07 10:34:28 +0800

    update coop content

--- app/src/helpers/CoopSet.js
+++ app/src/helpers/CoopSet.js
@@ -3,7 +3,7 @@ const coop_set = [
 		id:"coop-performance",
 		title: "Performance | 表演",
 		contentChinese: "好得舞蹈表演永遠不嫌少！透過搖擺舞、爵士樂，與Naughty Swing專業的演出能力，為你的記者會、舞會等活動增添美麗與歡樂的色彩！",
-		contentEnglish: " There's never too much of a good performance, especially when it comes to social dance! Want to add some elements which entertain your audience, invigorate your event and give you all a total enjoyment with profession and passion? We ARE just the one you're looking for!",
+		contentEnglish: "There's never too much of a good performance, especially when it comes to social dance! Want to add some elements which entertain your audience, invigorate your event and give you all a total enjoyment with profession and passion? We ARE just the one you're looking for!",
 		mediaTitle: "performance",
 		mediaSrc: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/coop%2Fcoop_01_performance_mini.jpg?alt=media&token=abf5550f-ffa4-424e-81d2-f9ca7783dfa6'
 	},
@@ -11,7 +11,7 @@ const coop_set = [
 		id: "coop-videoshooting",
 		title:"Video Production | 影片",
 		contentChinese: "影片所能傳遞的訊息勝過千言萬語，更遑論透過搖擺舞的影片拍攝，將肢體語言的豐富情感傳達至舞蹈影片中，帶領觀眾一同進步故事裡！",
-		contentEnglish:"'A picture says a thousand words.' - while a video shows a thousand images, and nothing's more contagious than a Swing Dance film! No words needed! we tell stories through jazz music and dance moves, as the chemistry among the dancers says it all. Happen to have something to be promoted through a good story? Tell us now!",
+		contentEnglish:"'A picture says a thousand words.' - while a video shows a thousand images, and nothing's more contagious than a Swing Dance film! We tell stories through jazz music and dance moves. Happen to have something to be promoted through a good story? Tell us now!",
 		mediaTitle: "i charleston taipei",
 		mediaSrc: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/coop%2Fcoop_02_videoproduction_mini.jpg?alt=media&token=a1b13952-3191-4d6c-af0e-706c15b05093'
 	},
@@ -19,7 +19,7 @@ const coop_set = [
 		id: "coop-teaching",
 		title:"Swing Dance Lessons | 搖擺舞專業教學",
 		contentChinese: "想要學習雙人社交舞蹈卻又不知該從何下手？快來參加我們的常態搖擺舞課程或是體驗課程，讓擁有專業舞蹈教學經歷的Naughty Swing，帶你進入搖擺舞的新世界！",
-		contentEnglish: "Excited but nervous, not sure when and how to step out of your comfort zone and onto the dance floor? Now is the time, and we're here to help you make that happen! Excited but nervous, not sure when and how to step out of your comfort zone and onto the dance floor? Now is the time, and we're here to help you make that happen! With our 10-year experience in dancing and diversified exposure to different dance styles, we understand the problems you may encounter and more important, the methods to overcome explained to you in an interesting and easily understood way!",
+		contentEnglish: "Not sure when and how to step out of your comfort zone and onto the dance floor? Now is the time! With our 10-year experience in dancing and diversified exposure to different dance styles, we will lead you into swing dance world in an interesting and easily understood way!",
 		mediaTitle: "當代藝術美術館體驗課程",
 		mediaSrc: "https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/coop%2Fcoop_03_lesson_mini.jpg?alt=media&token=b92a9fb9-f67e-4e14-9d02-41f049202e60"
 	}
